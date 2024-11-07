df22df7e37b7073b680b9da2c1e100c93beb07b2
Serhii Liesnykov <145987898+serdroi@users.noreply.github.com>
2024-11-07 11:35:41 +0100

    Update delete.php

--- product/delete.php
+++ product/delete.php
@@ -1,21 +1,21 @@
 <?php
 
-// HTTP-заголовки
+// HTTP-Header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// подключим файл для соединения с базой и объектом Product
+// Fügen wir die Datei zur Verbindung mit der Datenbank und dem Produktobjekt ein
 include_once "../config/database.php";
 include_once "../objects/product.php";
 
-// получаем соединение с БД
+// Wir bekommen eine Verbindung zur Datenbank
 $database = new Database();
 $db = $database->getConnection();
 
-// подготовка объекта
+// Vorbereitung der Baustelle
 $product = new Product($db);
 
 // получаем id товара
