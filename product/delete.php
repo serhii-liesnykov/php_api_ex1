82a35204198a781dac431070d581452cd815c564
Serhii Liesnykov <145987898+serdroi@users.noreply.github.com>
2024-11-08 21:09:22 +0100

    Update delete.php

--- product/delete.php
+++ product/delete.php
@@ -18,25 +18,25 @@ $db = $database->getConnection();
 // Vorbereitung der Baustelle
 $product = new Product($db);
 
-// получаем id товара
+// Wir erhalten die Produkt-ID
 $data = json_decode(file_get_contents("php://input"));
 
-// установим id товара для удаления
+// Legen Sie die zu löschende Produkt-ID fest
 $product->id = $data->id;
 
-// удаление товара
+// Produktentfernung
 if ($product->delete()) {
-    // код ответа - 200 ok
+    // Antwortcode - 200 ok
     http_response_code(200);
 
-    // сообщение пользователю
+    // Nachricht an den Benutzer
     echo json_encode(array("message" => "Товар был удалён"), JSON_UNESCAPED_UNICODE);
 }
-// если не удается удалить товар
+// wenn Sie das Produkt nicht löschen können
 else {
-    // код ответа - 503 Сервис не доступен
+    // Antwortcode – 503 Dienst nicht verfügbar
     http_response_code(503);
 
-    // сообщим об этом пользователю
+    // Wir werden den Nutzer hierüber informieren
     echo json_encode(array("message" => "Не удалось удалить товар"));
 }
