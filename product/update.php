37d151650037e4b9d4e9d70296f8e7184d340359
Serhii Liesnykov <145987898+serdroi@users.noreply.github.com>
2024-11-13 21:58:45 +0100

    Update update.php

--- product/update.php
+++ product/update.php
@@ -1,38 +1,38 @@
 <?php
 
-// HTTP-заголовки
+// HTTP-Header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// подключаем файл для работы с БД и объектом Product
+// Verbinden Sie die Datei, um mit der Datenbank und dem Produktobjekt zu arbeiten
 include_once "../config/database.php";
 include_once "../objects/product.php";
 
-// получаем соединение с базой данных
+// Wir bekommen eine Verbindung zur Datenbank
 $database = new Database();
 $db = $database->getConnection();
 
-// подготовка объекта
+// Vorbereitung der Baustelle
 $product = new Product($db);
 
-// получаем id товара для редактирования
+// Rufen Sie die Produkt-ID zur Bearbeitung ab
 $data = json_decode(file_get_contents("php://input"));
 
-// установим id свойства товара для редактирования
+// Legen Sie die Produkteigenschafts-ID zur Bearbeitung fest
 $product->id = $data->id;
 
-// установим значения свойств товара
+// Legen Sie die Werte der Produkteigenschaften fest
 $product->name = $data->name;
 $product->price = $data->price;
 $product->description = $data->description;
 $product->category_id = $data->category_id;
 
-// обновление товара
+// Produktaktualisierung
 if ($product->update()) {
-    // установим код ответа - 200 ok
+    // Setzen Sie den Antwortcode auf 200 ok
     http_response_code(200);
 
     // сообщим пользователю
